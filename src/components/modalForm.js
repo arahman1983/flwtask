7c56e564f80ac6053c34eca49f86bdd9ebeec1a7
Abdelrahman Elhussiny <Abdelrahman Elhussiny>
2020-11-05 16:52:42 +0200

    add validation to form

--- src/components/modalForm.js
+++ src/components/modalForm.js
@@ -1,26 +1,46 @@
-import React, {useState} from "react";
-import {Modal} from 'react-bootstrap'
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
 
-export default function FormModal ({handleHide, show}) {
-  const [employeeName, setEmployeeName] = useState('')
+export default function FormModal({ handleHide, show }) {
+  const [employeeName, setEmployeeName] = useState("");
+  const [nameValid, setNameValid] = useState(true);
+
   const handleClose = () => {
-    setEmployeeName('')
+    setEmployeeName("");
     handleHide();
+  };
+
+  const employeeNameChange = (e) => {
+    setNameValid(true);
+    setEmployeeName(e.target.value);
+  };
+  
+  const validateUser = () => {
+    if(employeeName.trim().length === 0 ){
+      setNameValid(false);
+      return ;
+    }
+      ///// logic to add employee to the server
+      setEmployeeName('')
+      handleHide()
   }
-  const employeeNameChange = (e) => setEmployeeName(e.target.value)
+
   return (
     <Modal show={show} onHide={handleClose} animation={false}>
       <Modal.Header closeButton>
-        <Modal.Title>MAdd Employee</Modal.Title>
+        <Modal.Title>Add Employee</Modal.Title>
       </Modal.Header>
       <Modal.Body>
-          <input className="form-control" type="text" value={employeeName} onChange={employeeNameChange} placeholder="Add Employees Full Name" />
+        <input className="form-control" type="text" value={employeeName} onChange={employeeNameChange} placeholder="Add Employees Full Name" />
+        <div className={`alert alert-danger ${nameValid && 'd-none'} my-3`} role="alert">
+          You must add Employee Name
+        </div>
       </Modal.Body>
       <Modal.Footer>
         <button className="btn btn-secondary" onClick={handleClose}>
           Close
         </button>
-        <button className="btn btn-primary" onClick={handleClose}>
+        <button className="btn btn-primary" onClick={validateUser}>
           Save Changes
         </button>
       </Modal.Footer>
